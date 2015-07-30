cd304b2d669b0d9a9237b519360a051eeadff32f
Magnus Suther <magnus.suther@gmail.com>
2015-07-30 22:56:20 +0200

    Move test/run_tests.dart to tools/run_tests.dart

--- src/tools/run_tests.dart
+++ src/tools/run_tests.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'package:path/path.dart' as path;
 
 // Run me with 'dart run_tests.dart'
 void main() {
@@ -7,8 +8,12 @@ void main() {
 
 runSuite(file) {
   print("Running unit tests for the ${file} suite ...");
+
+  var scriptDir = path.dirname(Platform.script.toFilePath());
+  var p = path.join(scriptDir, "../");
+
   Process.run('/usr/lib/dart/bin/pub', ['run', 'test', '-p', 'dartium', file],
-      workingDirectory: '../')
+      workingDirectory: p)
     .then((ProcessResult results) {
       print(results.stdout);
       print(results.stderr);
