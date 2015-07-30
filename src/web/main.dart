90d889586187df85965cc1c40244d4d7b4666ddf
Magnus Suther <magnus.suther@gmail.com>
2015-07-30 17:42:40 +0200

    main.dart: Split main() into two new functions, initialize() and importFromLocalStorage()

--- src/web/main.dart
+++ src/web/main.dart
@@ -3,19 +3,26 @@ part of magnussuther.chrome_extension_saml_decoder;
 MessageStore messageStore;
 Actions actions;
 
-void main() {
+initialize() {
   actions = new Actions();
-
   messageStore = new MessageStore();
+}
+
+importFromLocalStorage() {
+  if (window.localStorage["messages"] != null) {
+    List<Map> storedMessages = JSON.decode(window.localStorage["messages"]);
+    var samlMessages = storedMessages.map((e) => new SamlMessage.fromJson(e)).toList();
+    actions.addAllSamlMessages(samlMessages);
+  }
+}
+
+void main() {
+  initialize();
 
   reactClient.setClientConfiguration();
 
   render(appComponent({}), querySelector('#app-container'));
 
   // Import all messages from LocalStorage, that the background page has produced since we last displayed the GUI.
-  if (window.localStorage["messages"] != null) {
-    List<Map> storedMessages = JSON.decode(window.localStorage["messages"]);
-    var samlMessages = storedMessages.map((e) => new SamlMessage.fromJson(e)).toList();
-    actions.addAllSamlMessages(samlMessages);
-  }
+  importFromLocalStorage();
 }
