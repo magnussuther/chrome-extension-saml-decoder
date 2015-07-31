3ef378638918eb92be876868857a5ea459efaf81
Magnus Suther <magnus.suther@gmail.com>
2015-07-31 20:07:31 +0200

    Add a new icon in various sizes. Update the build.dart script to include these when packaging.

--- src/tools/build.dart
+++ src/tools/build.dart
@@ -9,5 +9,8 @@ void main() {
     print(results.stderr);
 
     new File(path.join(p, "manifest.json")).copy(path.join(p, "build", "manifest.json"));
+    new File(path.join(p, "icon_128.png")).copy(path.join(p, "build", "icon_128.png"));
+    new File(path.join(p, "icon_48.png")).copy(path.join(p, "build", "icon_48.png"));
+    new File(path.join(p, "icon_16.png")).copy(path.join(p, "build", "icon_16.png"));
   });
 }
