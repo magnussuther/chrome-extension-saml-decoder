111b66425105ba9e9c3eae549dd6d05019239821
Magnus Suther <magnus.suther@gmail.com>
2015-08-23 16:50:55 +0200

    Adjust the tool/build.dart script to build for both release and debug.

--- src/tool/build.dart
+++ src/tool/build.dart
@@ -4,13 +4,25 @@ import 'package:path/path.dart' as path;
 void main() {
   var scriptDir = path.dirname(Platform.script.toFilePath());
   var p = path.join(scriptDir, "../");
-  Process.run('/usr/lib/dart/bin/pub', ['build', '--mode', 'release'], workingDirectory: "${p}").then((ProcessResult results) {
+  Process.run('/usr/lib/dart/bin/pub', ['build', '--mode', 'release', '--output', 'build/release'], workingDirectory: "${p}").then((ProcessResult results) {
     print(results.stdout);
     print(results.stderr);
 
-    new File(path.join(p, "manifest.json")).copy(path.join(p, "build", "manifest.json"));
-    new File(path.join(p, "icon_128.png")).copy(path.join(p, "build", "icon_128.png"));
-    new File(path.join(p, "icon_48.png")).copy(path.join(p, "build", "icon_48.png"));
-    new File(path.join(p, "icon_16.png")).copy(path.join(p, "build", "icon_16.png"));
+    new File(path.join(p, "manifest.json")).copy(path.join(p, "build", "release", "manifest.json"));
+    new File(path.join(p, "icon_128.png")).copy(path.join(p, "build", "release", "icon_128.png"));
+    new File(path.join(p, "icon_48.png")).copy(path.join(p, "build", "release", "icon_48.png"));
+    new File(path.join(p, "icon_16.png")).copy(path.join(p, "build", "release", "icon_16.png"));
+
+
+    Process.run('/usr/lib/dart/bin/pub', ['build', '--mode', 'debug', '--output', 'build/debug'], workingDirectory: "${p}").then((ProcessResult results) {
+      print(results.stdout);
+      print(results.stderr);
+
+      new File(path.join(p, "manifest.json")).copy(path.join(p, "build", "debug", "manifest.json"));
+      new File(path.join(p, "icon_128.png")).copy(path.join(p, "build", "debug", "icon_128.png"));
+      new File(path.join(p, "icon_48.png")).copy(path.join(p, "build", "debug", "icon_48.png"));
+      new File(path.join(p, "icon_16.png")).copy(path.join(p, "build", "debug", "icon_16.png"));
+    });
+
   });
 }
