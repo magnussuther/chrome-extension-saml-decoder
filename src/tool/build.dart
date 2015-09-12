341d7ee25797228b54e80ec2c626a18d4e785ac1
Magnus Suther <magnus.suther@gmail.com>
2015-09-12 16:59:53 +0200

    Add a promotion image and a screeenshot. Also, update the build script to include these files in the builds.

--- src/tool/build.dart
+++ src/tool/build.dart
@@ -9,6 +9,8 @@ void main() {
     print(results.stderr);
 
     new File(path.join(p, "manifest.json")).copy(path.join(p, "build", "release", "manifest.json"));
+    new File(path.join(p, "promotion.png")).copy(path.join(p, "build", "release", "promotion.png"));
+    new File(path.join(p, "screenshot-1.png")).copy(path.join(p, "build", "release", "screenshot-1.png"));
     new File(path.join(p, "icon_128.png")).copy(path.join(p, "build", "release", "icon_128.png"));
     new File(path.join(p, "icon_48.png")).copy(path.join(p, "build", "release", "icon_48.png"));
     new File(path.join(p, "icon_16.png")).copy(path.join(p, "build", "release", "icon_16.png"));
@@ -20,6 +22,8 @@ void main() {
       print(results.stderr);
 
       new File(path.join(p, "manifest.json")).copy(path.join(p, "build", "debug", "manifest.json"));
+      new File(path.join(p, "promotion.png")).copy(path.join(p, "build", "debug", "promotion.png"));
+      new File(path.join(p, "screenshot-1.png")).copy(path.join(p, "build", "debug", "screenshot-1.png"));
       new File(path.join(p, "icon_128.png")).copy(path.join(p, "build", "debug", "icon_128.png"));
       new File(path.join(p, "icon_48.png")).copy(path.join(p, "build", "debug", "icon_48.png"));
       new File(path.join(p, "icon_16.png")).copy(path.join(p, "build", "debug", "icon_16.png"));
